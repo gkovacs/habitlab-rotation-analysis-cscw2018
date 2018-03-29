5907ab21da034c28a999436d1858b5ceda66c5e9
Geza Kovacs <geza0kovacs@gmail.com>
2018-03-29 01:25:45 -0700

    more preprocessing scripts

--- extract_dataframe.py
+++ extract_dataframe.py
@@ -1,6 +1,32 @@
 import pandas as pd
 from math import log
 
+def is_facebook(x):
+  return x['domain'] == 'www.facebook.com'
+
+def is_youtube(x):
+  return x['domain'] == 'www.youtube.com'
+
+default_facebook_interventions = [
+  'facebook/feed_injection_timer',
+  'facebook/remove_news_feed',
+  'facebook/remove_comments',
+  'facebook/toast_notifications',
+  'facebook/show_timer_banner',
+]
+
+default_youtube_interventions = [
+  'youtube/remove_sidebar_links',
+  'youtube/prompt_before_watch',
+  'youtube/remove_comment_section',
+]
+
+def is_default_facebook_intervention(x):
+  return x['intervention'] in default_facebook_interventions
+
+def is_default_youtube_intervention(x):
+  return x['intervention'] in default_youtube_interventions
+
 def extract_dataframe(alldata, filter_funcs=[]):
   if callable(filter_funcs):
     filter_funcs = [filter_funcs]
@@ -35,3 +61,39 @@ def extract_dataframe(alldata, filter_funcs=[]):
             if accept:
               rows.append(row)
   return pd.DataFrame(rows)
+
+def extract_dataframe_daily(alldata, filter_funcs=[]):
+  if callable(filter_funcs):
+    filter_funcs = [filter_funcs]
+  rows = []
+  for install_id,experiment_info_with_sessions in alldata.items():
+    for experiment_info in experiment_info_with_sessions:
+      for condition_info in experiment_info['condition_info_list']:
+        condition = condition_info['condition']
+        for day_info in condition_info['day_info_list']:
+          total_time_on_day = 0
+          for session_info in day_info['session_info_list']:
+            domain = session_info['domain']
+            userid = session_info['userid']
+            time_spent = session_info['time_spent']
+            timestamp = session_info['timestamp']
+            intervention = session_info['intervention']
+            #if domain != 'www.facebook.com':
+            #  continue
+            row = {
+              'log_time_spent': log(time_spent),
+              'time_spent': time_spent,
+              'install_id': install_id,
+              'userid': userid,
+              'condition': condition,
+              'intervention': intervention,
+              'domain': domain,
+            }
+            accept = True
+            for filter_func in filter_funcs:
+              if not filter_func(row):
+                accept = False
+            if accept:
+              rows.append(row)
+  return pd.DataFrame(rows)
+
--- linear_mixed_models.R
+++ linear_mixed_models.R
@@ -2,21 +2,41 @@
 #install.packages('stargazer')
 #install.packages('sjPlot')
 library(lme4)
-library(stargazer)
-library(sjPlot)
+#library(stargazer)
+#library(sjPlot)
 
 #data <- read.csv('/home/geza/habitlab_data_analysis/data_march28_2am.csv')
 #data <- read.csv('/home/geza/habitlab_data_analysis/data_march28_2am_facebook_only.csv')
+#data <- read.csv('/home/geza/habitlab_data_analysis/data_march28_2am_youtube_only.csv')
+#data <- read.csv('/home/geza/habitlab_data_analysis/data_march28_2am_youtube_only_default_interventions_only.csv')
 data <- read.csv('/home/geza/habitlab_data_analysis/data_march28_2am_facebook_only_default_interventions_only.csv')
 summary(data)
 #data <- data[data$domain == 'www.facebook.com', ]
 #summary(data)
 
 #results <- lmer(log_time_spent ~ as.factor(condition) + as.factor(intervention) + as.factor(domain) + (1|install_id), data = data)
-results <- lmer(log_time_spent ~ as.factor(condition) + as.factor(intervention) + (1|install_id), data = data)
-plot(results)
-#stargazer(results)
+#results <- lmer(log_time_spent ~ as.factor(condition) + as.factor(intervention) + (1|install_id), data = data)
+
+#results <- lmer(log_time_spent ~ as.factor(condition) + as.factor(intervention) + (1|install_id), data = data)
+#resultsnull <- lmer(log_time_spent ~ as.factor(intervention) + (1|install_id), data = data)
+#anova(resultsnull, results)
+
+results <- aov(log_time_spent ~ as.factor(intervention), data = data)
 summary(results)
-vcov(results)
-anova(results)
+
+results <- lmer(log_time_spent ~ as.factor(condition) + as.factor(intervention) + (1|install_id), data = data)
+resultsnull <- lmer(log_time_spent ~ as.factor(intervention) + (1|install_id), data = data)
+summary(results)
+anova(resultsnull, results)
+
+results <- lmer(log_time_spent ~ as.factor(condition) + as.factor(intervention) + (1|install_id), data = data)
+resultsnull <- lmer(log_time_spent ~ as.factor(condition) + (1|install_id), data = data)
+summary(results)
+anova(resultsnull, results)
+
+#plot(results)
+#stargazer(results)
+#summary(results)
+#vcov(results)
+#anova(results)
 #aov(results)
